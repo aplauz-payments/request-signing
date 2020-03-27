1f3cfca3604137edd83f1c5622bfdf33d1e634ce
Kevin Brown <kevin.brown@techin.site>
2020-03-27 12:10:21 +1100

    Node JS example

---
+++ nodejs/index.js
@@ -0,0 +1,35 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const { promisify } = require("util");
+const readFile = promisify(fs.readFile);
+
+(async () => {
+  const requestBody = JSON.stringify({
+    pin: "8888888888888888",
+    currencyCode: "EUR",
+    amount: 8,
+    channelId: "ecommerce",
+    transactionReference: "ABC",
+    deviceReference: "539e5805-b2eb-4ac5-9e5e-45ec0524172e",
+    integratorReference: "456"
+  });
+
+  // In a production deployment this needs to be stored securely and retrieved
+  // as needed, for example from AWS SecretsManager
+  const privateKey = await readFile("./example.private.key.pem", "utf8");
+
+  const signer = crypto.createSign("RSA-SHA256");
+  signer.write(requestBody);
+  const base64EncodedSignature = signer.sign(privateKey).toString("base64");
+
+  console.log("---------------");
+  console.log("Node JS example");
+  console.log("---------------");
+  console.log("With the request body being:");
+  console.log(requestBody);
+  console.log();
+  console.log("And using the private key in ./example.private.key.pem");
+  console.log();
+  console.log("Set the Shared-Key header to:");
+  console.log(base64EncodedSignature);
+})();
