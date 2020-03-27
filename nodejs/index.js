2a0eebfcebbb5821c081f6bcbdffb497dc7f43b3
Kevin Brown <kevin.brown@techin.site>
2020-03-27 12:13:19 +1100

    Pulled settings to the top of the file for ease of use.

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,20 +3,24 @@ const fs = require("fs");
 const { promisify } = require("util");
 const readFile = promisify(fs.readFile);
 
+// Settings
+const privateKeyPath = "./example.private.key.pem";
+const body = {
+  pin: "8888888888888888",
+  currencyCode: "EUR",
+  amount: 8,
+  channelId: "ecommerce",
+  transactionReference: "ABC",
+  deviceReference: "539e5805-b2eb-4ac5-9e5e-45ec0524172e",
+  integratorReference: "456"
+};
+
 (async () => {
-  const requestBody = JSON.stringify({
-    pin: "8888888888888888",
-    currencyCode: "EUR",
-    amount: 8,
-    channelId: "ecommerce",
-    transactionReference: "ABC",
-    deviceReference: "539e5805-b2eb-4ac5-9e5e-45ec0524172e",
-    integratorReference: "456"
-  });
+  const requestBody = JSON.stringify(body);
 
   // In a production deployment this needs to be stored securely and retrieved
   // as needed, for example from AWS SecretsManager
-  const privateKey = await readFile("./example.private.key.pem", "utf8");
+  const privateKey = await readFile(privateKeyPath, "utf8");
 
   const signer = crypto.createSign("RSA-SHA256");
   signer.write(requestBody);
@@ -28,7 +32,7 @@ const readFile = promisify(fs.readFile);
   console.log("With the request body being:");
   console.log(requestBody);
   console.log();
-  console.log("And using the private key in ./example.private.key.pem");
+  console.log(`And signing with the private key in ${privateKeyPath}`);
   console.log();
   console.log("Set the Shared-Key header to:");
   console.log(base64EncodedSignature);
